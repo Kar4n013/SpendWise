6bbeb81474f31c5e05c4c729cac0fd036d886cb4
Kar4n013 <karangiri01304@gmail.com>
2026-03-12 20:58:38 +0530

    database connection established

---
+++ SECUREBANK/src/main/java/dbInfo/DataReader.java
@@ -0,0 +1,25 @@
+package dbInfo;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class DataReader {
+	public static String[] dataReader() throws FileNotFoundException {
+		FileReader fileReader = new FileReader(
+				"C:\\Users\\Karan Giri\\eclipse-workspace\\Projects\\BankingSystem\\src\\main\\java\\dbInfo\\Info.txt");
+		Properties properties = new Properties();
+		try {
+			properties.load(fileReader);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return new String[] { properties.getProperty("username"), properties.getProperty("password"),
+				properties.getProperty("db_name") };
+
+	}
+
+}
